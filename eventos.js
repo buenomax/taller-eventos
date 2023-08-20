1e2a1c6067bf3a7b1a10cf5a118644e296141cf5
Max Bueno <105615781+buenomax@users.noreply.github.com>
2023-08-20 03:04:12 -0300

    se arregla error del click

--- eventos.js
+++ eventos.js
@@ -6,3 +6,12 @@ const divContenedor = document.getElementById('contenedor');
 divContenedor.addEventListener('click', () => {
     alert('Hola! Soy el div');
 });
+
+let buton = document.getElementById("saludarBtn");
+
+buton.addEventListener("click", (event) => {
+
+event.stopPropagation();
+
+
+});
